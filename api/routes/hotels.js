23f9d017d55732dccbc7635efaed72d2554b8a3b
Melaku Demeke <melaku.demeke789@gmail.com>
2023-03-27 17:30:32 +0300

    TASK LIST

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,8 +2,13 @@ import express from "express"
 
 const router = express.Router()
 
-//TODO add CRETATE UPDATE DELETE routes
+//TODO add CRETATE UPDATE DELETE GET GETALL routes
 
+// CREATE
+// UPDATE
+// DELETE
+// GET
+// GET ALL
 
 
 export default router
