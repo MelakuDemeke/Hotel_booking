7b885dc7eb0e084ca2f3bc64101ca97b20f53e90
Melaku Demeke <melaku.demeke789@gmail.com>
2023-03-27 16:51:11 +0300

    connection to mongo added

--- api/index.js
+++ api/index.js
@@ -1,11 +1,26 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 const app = express()
 dotenv.config()
+
+
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log("connected to mongo db")
+    }catch(error){
+        throw error
+    }
+};
+
 
 app.get("/", (req, res)=>{
     res.send("Hello")
 })
 
-app.listen(8100, ()=>{console.log("server started at http://localhost:8100")})
+app.listen(8100, ()=>{
+    connect()
+    console.log("server started at http://localhost:8100")
+})
