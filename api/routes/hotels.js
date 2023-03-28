f7663461c2f3f153662729ae45c9981c8ee30ec4
Melaku Demeke <melaku.demeke789@gmail.com>
2023-03-28 17:08:06 +0300

    hotel delete api add

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -31,6 +31,16 @@ router.put("/:id", async (req, res) => {
     }
 })
 // DELETE
+router.delete("/:id", async (req, res) => {
+    try{
+        await Hotel.findByIdAndDelete(
+            req.params.id,
+            );
+        res.status(200).json("Hotel has been deleted")
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 // GET
 // GET ALL
 
