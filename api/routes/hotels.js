00ec7e7945eeb543df88c7dec604c31d21ea6786
Melaku Demeke <melaku.demeke789@gmail.com>
2023-04-12 15:34:05 +0300

    get all added

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -54,6 +54,13 @@ router.get("/:id", async (req, res) => {
     }
 })
 // GET ALL
-
+router.get("/",async (req, res) => {
+    try{
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels)
+    }catch(err){
+        res.status(500).json(err);
+    };
+})
 
 export default router
